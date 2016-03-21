54439f957fdb9dd4fa6c8426f1e6d26366675a02
restard <drestard@gmail.com>
2016-03-22 02:39:04 +0900

    Refs #31 【PC/SP - JP/EN - DVD注文】2番目以降もタイトルと枚数、どちらかのみ値があるときにもエラーを返す .contact_film_kind .contact_film_unitにeach関数で処理

--- wp/wp-content/themes/petiteadventurefilms/assets/js/form_order.js
+++ wp/wp-content/themes/petiteadventurefilms/assets/js/form_order.js
@@ -229,12 +229,12 @@ $(function(){
 			jVal.text("#contact_tel");
 			jVal.emailConfirm("#contact_email_confirm");
 			jVal.radio("input[name='contact_pay_way']");
-			//jVal.cstmOrderCheck();
-			jVal.cstmOrder(".contact_film_kind");
-			jVal.cstmOrder(".contact_film_unit");
-
-	//$(".contact_film_kind").blur();
-	//$(".contact_film_unit").blur(jVal.cstmOrder);
+			$(".contact_film_kind").each(function(){
+				jVal.cstmOrder($(this));
+			});
+			$(".contact_film_unit").each(function(){
+				jVal.cstmOrder($(this));
+			});
 			jVal.sendIt();
 		});
 
