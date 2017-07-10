a8bab7e7802d6191d96fbc09dca2c95e7a21b8bb
drestard@gmail.com <drestard@gmail.com>
2017-07-11 00:05:26 +0900

    Refs #52 【PC/SP - JP/EN】コンテンツの無断転載禁止に関して記載する リンク先修正

--- wp/wp-content/themes/petiteadventurefilms/footer.php
+++ wp/wp-content/themes/petiteadventurefilms/footer.php
@@ -6,7 +6,7 @@
 	<div class="col col_9 last">
 		<ul>
 			<li><a href="<?php echo get_permalink(get_page_by_path("specified-commercial-transaction-law")); ?>">特定商取引法に基づく表記</a></li>
-			<li><a href="<?php echo get_permalink(get_page_by_path("terms-of-service")); ?>">サイトのご利用について</a></li>
+			<li><a href="<?php echo get_permalink(get_page_by_path("copyrights-disclaimers")); ?>">サイトのご利用について</a></li>
 			<li><a href="<?php echo get_permalink(get_page_by_path("gathering-the-personal-information")); ?>">個人情報の扱いについて</a></li>
 			<li><a href="<?php echo get_permalink(get_page_by_path("privacy-policy")); ?>">プライバシーポリシー</a></li>
 			<li><a href="<?php echo get_permalink(get_page_by_path("system-requirements")); ?>">推奨環境</a></li>
