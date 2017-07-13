76d57610881d1b5097a2c4cf5c3fdf3f06fc8c19
drestard@gmail.com <drestard@gmail.com>
2017-07-13 20:18:58 +0900

    Refs #30 【PC/SP - JP/EN】YouTube動画ページ開発 Youtubeアイコンにテキスト挿入

--- wp/wp-content/themes/petiteadventurefilms/archive-channel.php
+++ wp/wp-content/themes/petiteadventurefilms/archive-channel.php
@@ -46,7 +46,7 @@ get_header(); ?>
 
 	<div class="col col_9 last">
 	<p class="m7_t">これまでにYouTubeで公開した映像の中から、厳選した動画をご紹介します。</p>
-	プチ・アドベンチャー・フィルムズの<span class="icon icon-youtube"></span>全動画は<a href="https://www.youtube.com/user/petiteadventurefilms" rel="nofollow" target="_blank">こちら</a>から
+	プチ・アドベンチャー・フィルムズの<span class="icon icon-youtube"><span class="display-none">YouTube</span></span>全動画は<a href="https://www.youtube.com/user/petiteadventurefilms" rel="nofollow" target="_blank">こちら</a>から
 	</div>
 
 	<?php
