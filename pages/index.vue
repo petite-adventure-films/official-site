16ba409a0766b7adcacbe164e0746d54b371b788
restard <restard653@gmail.com>
2020-07-29 17:05:03 +0900

    2.0 development environment

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+	<div>
+		test中
+	</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
---
+++ store/index.js
@@ -0,0 +1,51 @@
+import { createClient } from '@/plugins/contentful'
+// import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
+
+const client = createClient();
+
+const contentTypes = ['news', 'post', 'event'];
+
+export const state = () => ({
+	post: []
+})
+
+export const getters = {
+	linkTo: () => (name, obj) => {
+		return { name: `${name}-slug`, params: { slug: obj.fields.slug } }
+	}
+
+	, dateFormat: () => (date) => {
+		let year  = new Date(date).getFullYear();
+		let month = new Date(date).getMonth() + 1;
+		let day   = new Date(date).getDate();
+		return year + '-' + ('00' + month).slice(-2) + '-' + ('00' + day).slice(-2);
+	}
+
+	// , renderRichText: () => (contents) => {
+	// 	return documentToHtmlString(contents);
+	// }
+}
+
+export const mutations = {
+	setPosts: (state, payload) => {
+		let type = payload.sys.contentType.sys.id;
+		if(state[type] === undefined)
+		{
+			state[type] = new Array();
+		}
+		state[type].push(payload);
+	}
+}
+
+export const actions = {
+	async getAllPosts({commit})
+	{
+		await client.getEntries()
+			.then(res => {
+				res.items.forEach((post) => {
+					commit('setPosts', post);
+				})
+			})
+			.catch(console.error)
+	}
+}
