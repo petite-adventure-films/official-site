cc6fadc20f6e09cbba3263e9f8fbb37157556c08
restard <restard653@gmail.com>
2020-08-20 18:54:37 +0900

    ショップテスト17

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,6 +1,6 @@
 <template>
 	<div>
-		<form ref="form" name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field" action="/thankyou">
+		<form ref="form" name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
 			<input type="hidden" name="form-name" value="contact">
 			<input type="hidden" name="bot-field">
 			<label>Your Name: <input type="text" name="name" /></label><br>
@@ -14,15 +14,22 @@
 <script>
 export default {
 
+	data: function()
+	{
+		return {
+
+		}
+	}
+
 	methods: {
 		submit: function()
 		{
-			let params = {
-				name: 'restard'
-				, email: 'email'
-				, 'form-name': 'contact'
-			}
-			this.$axios.$post('https://old-state--dreamy-goldberg-f608e5.netlify.app', params)
+			const params = new URLSearchParams();
+			params.append('form-name', 'contact');
+			params.append('username', 'ress');
+			params.append('message', 'msg');
+
+			this.$axios.$post('/', params)
 				.then((res) => {
 					console.log('done')
 				})
