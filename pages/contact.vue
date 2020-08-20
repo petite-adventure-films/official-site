49cc68caa66437de1e797424bdff3bdb0166e650
restard <restard653@gmail.com>
2020-08-20 16:10:35 +0900

    ショップテスト4

---
+++ pages/contact.vue
@@ -0,0 +1,46 @@
+<template>
+	<div>
+		<form name="contact" method="POST" netlify>
+			<input type="hidden" name="form-name" value="contact">
+			<p>
+				<label>Your Name: <input type="text" name="name" /></label>
+			</p>
+			<p>
+				<label>Your Email: <input type="email" name="email" /></label>
+			</p>
+			<p>
+				<label>Your Role: <select name="role[]" multiple>
+					<option value="leader">Leader</option>
+					<option value="follower">Follower</option>
+				</select></label>
+			</p>
+			<p>
+				<label>Message: <textarea name="message"></textarea></label>
+			</p>
+			<p>
+				<button type="submit">Send</button>
+			</p>
+		</form>
+	</div>
+</template>
+
+<script>
+import { mapState} from 'vuex'
+import cardNews from '@/components/card_news'
+import cardFilm from '@/components/card_film'
+import cardPost from '@/components/card_post'
+
+export default {
+
+	components: {
+		cardNews, cardFilm, cardPost
+	}
+
+	, computed: {
+		...mapState(['news', 'film', 'post'])
+		, sortedNews: function(){
+			return this.news.slice(0, 3)
+		}
+	}
+}
+</script>
--- pages/shop/buy.vue
+++ pages/shop/buy.vue
@@ -54,27 +54,6 @@
 				</v-card>
 			</v-dialog>
 
-			<form name="contact" method="POST" netlify>
-  <p>
-    <label>Your Name: <input type="text" name="name" /></label>
-  </p>
-  <p>
-    <label>Your Email: <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <label>Your Role: <select name="role[]" multiple>
-      <option value="leader">Leader</option>
-      <option value="follower">Follower</option>
-    </select></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
-
 			<v-btn block color="purple" class="mt-2" @click="completeOrder">注文確定</v-btn>
 		</v-stepper-content>
 
