e30ddbb9877a7c07b7648cf5447317eb22d538b7
restard <restard653@gmail.com>
2020-08-20 09:50:30 +0900

    netlify設定テスト3

--- .nuxt/middleware.js
+++
@@ -1,6 +0,0 @@
-const middleware = {}
-
-middleware['getContentful'] = require('../middleware/getContentful.js')
-middleware['getContentful'] = middleware['getContentful'].default || middleware['getContentful']
-
-export default middleware
