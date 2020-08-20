be466f1e0ab63ed319327dbd0346f1db3857a1a3
restard <restard653@gmail.com>
2020-08-20 15:54:34 +0900

    ショップテスト3

--- pages/shop/buy.vue
+++ pages/shop/buy.vue
@@ -54,9 +54,26 @@
 				</v-card>
 			</v-dialog>
 
-			<v-form data-netlify="true">
-				<v-btn @click="submit">POST</v-btn>
-			</v-form>
+			<form name="contact" method="POST" netlify>
+  <p>
+    <label>Your Name: <input type="text" name="name" /></label>
+  </p>
+  <p>
+    <label>Your Email: <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <label>Your Role: <select name="role[]" multiple>
+      <option value="leader">Leader</option>
+      <option value="follower">Follower</option>
+    </select></label>
+  </p>
+  <p>
+    <label>Message: <textarea name="message"></textarea></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
 
 			<v-btn block color="purple" class="mt-2" @click="completeOrder">注文確定</v-btn>
 		</v-stepper-content>
