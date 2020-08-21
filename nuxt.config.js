ce72abcba563f53b17ba229d6f6c296b930fcd10
restard <restard653@gmail.com>
2020-08-21 15:55:39 +0900

    ショップテスト24

--- nuxt.config.js
+++ nuxt.config.js
@@ -98,7 +98,7 @@ export default {
 		  CTF_SPACE_ID             : process.env.CTF_SPACE_ID
 		, CTF_CDA_ACCESS_TOKEN     : process.env.CTF_CDA_ACCESS_TOKEN
 		, CTF_PREVIEW_ACCESS_TOKEN : process.env.CTF_PREVIEW_ACCESS_TOKEN
-		, FUNCTION_URL : process.env.URL || 'http://localhost:7000'
+		, FUNCTION_URL : process.env.URL || 'http://localhost:9000'
 		, OAUTH_USER : process.env.OAUTH_USER
 		, OAUTH_CLIENT_ID : process.env.OAUTH_CLIENT_ID
 		, OAUTH_CLIENT_SECRET : process.env.OAUTH_CLIENT_SECRET
