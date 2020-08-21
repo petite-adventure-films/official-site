1d14d423e005d4b78655832612f09bc002f03384
restard <restard653@gmail.com>
2020-08-21 22:18:24 +0900

    ショップテスト36

--- functions/api/submission-created.js
+++ functions/api/submission-created.js
@@ -31,7 +31,7 @@ exports.handler = function(event, context, callback) {
 
 	let mailOptions = {
 		from    : `petite adventure films <info@petiteadventurefilms.com>`,
-		to      :  `${email}`,
+		to      :  'drestard@gmail.com',
 		subject : 'testありがとうございます',
 		text    : `${JSON.stringify(body)} ${JSON.stringify(data)} ありがとうございます`
 	};
