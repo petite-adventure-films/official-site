7ff9a5b9533fe603e6d8185a106524c987abf93c
restard <restard653@gmail.com>
2020-12-31 16:43:33 +0900

    キャッシュ削除

--- wp/wp-content/themes/petiteadventurefilms/_contact_post.php
+++
@@ -1,7 +0,0 @@
-<?php
-$contact_kind = $_POST["contact_kind"];
-$contact_name = $_POST["contact_name"];
-$contact_email = $_POST["contact_email"];
-$contact_email_confirm = $_POST["contact_email_confirm"];
-$contact_contents = $_POST["contact_contents"];
-?>
