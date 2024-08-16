58292d1062f2697f3f740e7bfe19134861959f64
restard <restard653@gmail.com>
2024-08-16 19:00:54 +0900

    profile写真修正

--- html/src/pages/director/index.vue
+++ html/src/pages/director/index.vue
@@ -32,7 +32,7 @@ for (const film of FILMS) {
       <img
         src="~/assets/images/director/profile.jpg"
         alt="早川由美子"
-        class="w-40 h-40 mt-4"
+        class="mt-4"
       />
       <p class="mt-4">
         1975年東京都出身。成蹊大学法学部、London School of
