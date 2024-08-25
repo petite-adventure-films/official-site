0e0988c0b8aeaf8bfde648ede8253004112a2464
restard <restard653@gmail.com>
2024-08-25 11:17:23 +0900

    検証のためにvar_dump

--- html/wp/wp-content/themes/petiteadventurefilms/functions.php
+++ html/wp/wp-content/themes/petiteadventurefilms/functions.php
@@ -62,6 +62,7 @@ function dispatch_github_actions()
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
   $response = curl_exec($ch);
   var_dump($response);
+  var_dump(GITHUB_TOKEN);
   curl_close($ch);
   return $response;
 }
