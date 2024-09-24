6f9569ee41d021020d0318886fcdfa65b47a545c
restard <restard653@gmail.com>
2024-09-24 20:30:25 +0900

    errorページwarning修正

--- html/src/error.vue
+++ html/src/error.vue
@@ -1,29 +1,22 @@
 <script setup>
-definePageMeta({
-  layout: false,
-});
 const error = useError();
 </script>
 
 <template>
-  <NuxtLayout name="post">
-    <template #h2>
-      {{
+    <h2>{{
         error.statusCode === 404
           ? 'ページが見つかりません'
-          : 'エラーが発生しました'
-      }}
-    </template>
+          : `${error.statusCode} エラーが発生しました`
+      }}</h2>
     <div v-if="error.statusCode === 404">
       <p>
         URLに間違いがないかもう一度ご確認ください。<br />もしくはページが一時的にアクセスができない状況にあるか、移動もしくは削除された可能性があります。
       </p>
     </div>
     <div v-else>
-      <p>しばらくしてからもう一度お試しください。</p>
+      <p>しばらくしてからもう一度お試しください。{{ error.message }}</p>
     </div>
     <NuxtLink to="/?op=skip" class="inline-block mt-4 link-text"
       >HOMEへ戻る</NuxtLink
     >
-  </NuxtLayout>
 </template>
